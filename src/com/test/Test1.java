4afddecf74b9eccc6a743ce165f568b9bc1d5c3f
ddmoorthy <dakshina.deenadayalan@sunoida.com>
2023-07-12 15:18:25 +0530

    DD Commit

--- src/com/test/Test1.java
+++ src/com/test/Test1.java
@@ -2,7 +2,19 @@ package com.test;
 
 public class Test1 {
 
-	public static void main(String[] args) {
+public static void main2main(String[] args) {
+		System.out.println("DD1");
 		System.out.println("First Test GitHub");
+		System.out.println("DD");
+	}
+	public static void main1main(String[] args) {
+		System.out.println("DD1");
+		System.out.println("First Test GitHub");
+		System.out.println("DD");
+	}
+	public static void main2(String[] args) {
+		System.out.println("DD1");
+		System.out.println("First Test GitHub");
+		System.out.println("DD");
 	}
 }
